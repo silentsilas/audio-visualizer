081c73ca7528c8ebaeb9cbfbde521242a61e1328
Silas <me@silentsilas.com>
2022-03-10 22:05:46 -0500

    import gltf model, modulate light intensity, add linting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log("test");
+import { Scene, PerspectiveCamera, WebGLRenderer, PointLight } from "three";
+import { Load } from "./model";
+
+let renderer, scene, camera, light;
+init().then(() => animate());
+
+async function init() {
+  const container = document.getElementById("container");
+
+  scene = new Scene();
+  camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+  camera.position.z = 5;
+
+  light = new PointLight(0x119911, 0);
+  light.counter = 0;
+  light.position.set(0, 0.2, 0.2);
+  scene.add(light);
+
+  renderer = new WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+  window.addEventListener("resize", onWindowResize);
+
+  try {
+    const gltf = await Load();
+    document.getElementById("loader").innerHTML = "";
+
+    // remove second hand with text above it
+    const objToRemove = gltf.scene.getObjectByName("Object_3");
+    objToRemove.parent.remove(objToRemove);
+
+    // turn remaining hand into wireframe
+    const hand = gltf.scene.getObjectByName("Object_4");
+    hand.material.wireframe = true;
+
+    // center hand in scene
+    hand.position.x = hand.position.x + 1.5;
+
+    scene.add(gltf.scene);
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  // modulate light intensity between 0.5 and 1.5
+  light.counter += 0.01;
+  light.intensity = Math.sin(light.counter) / 2 + 1;
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
---
+++ src/model.ts
@@ -0,0 +1,30 @@
+import { AnimationClip, Scene, Camera } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+const loader = new GLTFLoader();
+
+export interface GLTF {
+  animations: AnimationClip[];
+  scene: Scene;
+  scenes: Scene[];
+  cameras: Camera[];
+  asset: object;
+}
+
+export function Load(): Promise<GLTF> {
+  const loadingDiv = document.getElementById("loader");
+  return new Promise((resolve, reject) => {
+    loader.load(
+      "/static/scene.gltf",
+      (object: GLTF) => resolve(object),
+      (progress) =>
+        (loadingDiv.innerHTML = `Loading model: ${
+          (progress.loaded / progress.total) * 100
+        }%`),
+      (error: ErrorEvent) => {
+        console.log(error.target);
+        reject(error);
+      }
+    );
+  });
+}
